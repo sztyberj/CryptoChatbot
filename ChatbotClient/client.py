b85cf62a00b74cb9c9c2b4ca9843d88762c25e5c
Jakub Sztyber <54943867+sztyberj@users.noreply.github.com>
2021-12-29 20:16:01 +0100

    Add files via upload

---
+++ ChatbotClient/client.py
@@ -0,0 +1,29 @@
+import socket
+
+HEADER = 512
+PORT = 5050
+FORMAT = 'utf-8'
+DISCONNECT_MESSAGE = "!DISCONNECT"
+SERVER = "192.168.43.179"
+ADDR = (SERVER, PORT)
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(ADDR)
+
+
+def send_message(msg):
+    message = msg.encode(FORMAT)
+    msg_length = len(message)
+    send_length = str(msg_length).encode(FORMAT)
+    send_length += b' ' * (HEADER - len(send_length))
+    client.send(send_length)
+    client.send(message)
+
+print("Connected! Use only keywords for more accurate answers.")
+while True:
+    msg = input()
+    if msg:
+        send_message(msg)
+    msg_length = client.recv(HEADER).decode(FORMAT)
+    if msg_length:
+        print(msg_length)
