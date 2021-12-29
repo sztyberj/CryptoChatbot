6dab839c047e2d53f151c7686183cf7666a022ba
Jakub Sztyber <54943867+sztyberj@users.noreply.github.com>
2021-12-29 20:16:29 +0100

    Update client.py

--- ChatbotClient/client.py
+++ ChatbotClient/client.py
@@ -4,7 +4,7 @@ HEADER = 512
 PORT = 5050
 FORMAT = 'utf-8'
 DISCONNECT_MESSAGE = "!DISCONNECT"
-SERVER = "192.168.43.179"
+SERVER = "localhost"
 ADDR = (SERVER, PORT)
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
