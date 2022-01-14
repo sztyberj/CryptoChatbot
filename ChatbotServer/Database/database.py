0834259f5a3bc3fbdd41e0f752b6b311e239d585
Jakub Sztyber <54943867+sztyberj@users.noreply.github.com>
2022-01-14 11:33:07 +0100

    Add files via upload

--- ChatbotServer/Database/database.py
+++ ChatbotServer/Database/database.py
@@ -3,7 +3,27 @@ import pyodbc
 SERVER = 'tcp:serverforchatbot.database.windows.net,1433'
 DATABASE = 'chatbot_db'
 USERNAME = 'user'
-PASSWORD = ''
+PASSWORD = 'JIPPprojekthasło12@'
 
 cnxn = pyodbc.connect('Driver={ODBC Driver 17 for SQL Server};Server='+SERVER+';Database='+DATABASE+';Uid='+USERNAME+';Pwd={'+PASSWORD+'};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;')
 cursor = cnxn.cursor()
+
+class Database:
+    def __init__(self):
+        Database.create_database(self)
+
+    def create_database(self):
+        try:
+            cursor.execute('CREATE TABLE Glossary (key_desc varchar(50), val_desc varchar(MAX));')
+            cursor.commit()
+        except:
+            pass
+
+        try:
+            cursor.execute('CREATE TABLE Cryptocurrency (Name varchar(25), ShortName varchar(8), Price money, Date date);')
+            cursor.commit()
+        except:
+            pass
+
+if __name__ == '__main__':
+    new = Database()
--- ChatbotServer/Database/db_operations.py
+++ ChatbotServer/Database/db_operations.py
@@ -1,24 +1,5 @@
 import ChatbotServer.Database.database as db
 
-class Database:
-    def __init__(self):
-        Database.create_database(self)
-
-
-    def create_database(self):
-        try:
-            db.cursor.execute('CREATE TABLE Glossary (key_desc varchar(50), val_desc varchar(MAX));')
-            db.cursor.commit()
-        except:
-            pass
-
-        try:
-            db.cursor.execute('CREATE TABLE Cryptocurrency (Name varchar(25), ShortName varchar(8), Price money NULL, Date date);')
-            db.cursor.commit()
-        except:
-            pass
-
-
 def insert_prices(name, data):
     query = "INSERT INTO dbo."+name+" VALUES (?, ?, ?, ?);"
     db.cursor.executemany(query, data)
@@ -48,12 +29,16 @@ def delete_all(name):
     db.cursor.execute(query)
     db.cursor.commit()
 
-def select_all(name):
+def select_all_gloss(name):
     query = f"SELECT key_desc, val_desc FROM dbo.{name};"
     db.cursor.execute(query)
     result = db.cursor.fetchall()
     return(result)
 
+def select_all_prices(name, date):
+    query = f"SELECT Name, ShortName, Price FROM dbo.{name} WHERE Date = '{date}';"
+    db.cursor.execute(query)
+    result = db.cursor.fetchall()
+    return(result)
 
-if __name__ == '__main__':
-    new = Database()
+
