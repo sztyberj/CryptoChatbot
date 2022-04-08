b8a2435e34c3ce05f12e938de1e283b655a7c4d7
Jakub Sztyber <54943867+sztyberj@users.noreply.github.com>
2022-04-08 23:24:01 +0200

    Update database.py

--- ChatbotServer/Database/database.py
+++ ChatbotServer/Database/database.py
@@ -2,8 +2,8 @@ import pyodbc
 
 SERVER = 'tcp:serverforchatbot.database.windows.net,1433'
 DATABASE = 'chatbot_db'
-USERNAME = 'user'
-PASSWORD = 'JIPPprojekthasło12@'
+USERNAME = ''
+PASSWORD = ''
 
 cnxn = pyodbc.connect('Driver={ODBC Driver 17 for SQL Server};Server='+SERVER+';Database='+DATABASE+';Uid='+USERNAME+';Pwd={'+PASSWORD+'};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;')
 cursor = cnxn.cursor()
